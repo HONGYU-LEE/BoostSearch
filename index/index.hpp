9ea63a4a6750af93caeda82c3a31075a9f3574f1
HONGYU-LEE <756687451@qq.com>
2020-09-07 20:27:23 +0800

    实现索引和搜索模块

--- index/index.hpp
+++ index/index.hpp
@@ -1,8 +1,8 @@
-#progma once
+#pragma once
 
 #include"../common/util.hpp"
 
-namespace index
+namespace indexes
 {
     //正排索引：根据文档ID来查找文档正文
     //倒排索引：根据文档正文(词)来找到文档ID
@@ -33,41 +33,32 @@ namespace index
         unordered_map<string, InvertedList> invert_index;   //倒排索引
         cppjieba::Jieba jieba;  //结巴分词
 
-        void buildForwardIndex();  
-        void buildInvertIndex();   
+        DocInfo* buildForwardIndex(string line);
+        void buildInvertIndex(const DocInfo& doc_info);
         
         public:
         Index();
         DocInfo* getDocInfo(int64_t doc_id);
         InvertedList* getInvertedList(const string& key);
-        void Build(); 
+        bool Build(const string& input_path);
         void CutWord(const string& input, vector<string>* output);
-    }
+    };
 
     //结巴分词词典位置
-    const char* const DICT_PATH = "../jieba_dict/jieba.dict.utf8";
-    const char* const HMM_PATH = "../jieba_dict/hmm_model.utf8";
-    const char* const USER_DICT_PATH = "../jieba_dict/user.dict.utf8";
-    const char* const IDF_PATH = "../jieba_dict/idf.utf8";
-    const char* const STOP_WORD_PATH = "../jieba_dict/stop_words.utf8";
+    const char* const DICT_PATH = "../cppjieba/dict/jieba.dict.utf8";
+    const char* const HMM_PATH = "../cppjieba/dict/hmm_model.utf8";
+    const char* const USER_DICT_PATH = "../cppjieba/dict/user.dict.utf8";
+    const char* const IDF_PATH = "../cppjieba/dict/idf.utf8";
+    const char* const STOP_WORD_PATH = "../cppjieba/dict/stop_words.utf8";
 
     Index::Index() 
         : jieba(DICT_PATH, HMM_PATH, USER_DICT_PATH, IDF_PATH, STOP_WORD_PATH) 
     {}
 
     //分词
-    void CutWord(const string& input, vector<string>* output)
+    void Index::CutWord(const string& input, vector<string>* output)
     {
-        jieba.CutForSearch(input, output);
-    }
-
-    //构建索引
-    void Index::Build()
-    {
-        //构建正排索引
-        buildForwardIndex();
-        //构建倒排索引
-        buildInvertIndex();
+        jieba.CutForSearch(input, *output);
     }
 
     //构建正排索引
@@ -75,15 +66,16 @@ namespace index
     {
         vector<string> tokens;
 
-        common::Util::Split(line, "\1", tokens);//根据\1划分内容
+        common::Util::Split(line, "\1", &tokens);//根据\1划分内容
         if(tokens.size() != 3)
         {
             //如果划分出来的属性不等于三个，说明数据有问题
+            cout << "数据划分错误！！！" << endl;
             return nullptr;
         }
 
         DocInfo doc_info;
-        doc_info.id = forward_index.size();
+        doc_info.doc_id = forward_index.size();
         doc_info.title = tokens[0];
         doc_info.url = tokens[1];
         doc_info.content = tokens[2];
@@ -101,12 +93,18 @@ namespace index
         {
             int title_count; //标题出现频率
             int content_count;   //正文出现频率
-        }
-        unordered_map<string, WordFrequency> word_count_map; //利用哈希表进行统计
+
+            WordCount() 
+                : title_count(0)
+                , content_count(0)
+            {}
+        };
+
+        unordered_map<string, WordCount> word_count_map; //利用哈希表进行统计
 
         //对标题进行分词
         vector<string> title_tokens;
-        CutWord(doc_info.title, title_tokens);
+        CutWord(doc_info.title, &title_tokens);
 
         //统计词频，这里统一将单词转为小写
         for(string word : title_tokens)
@@ -117,9 +115,9 @@ namespace index
 
         //对正文进行分词
         vector<string> content_tokens;
-        CutWord(doc_info.content, content_tokens);
+        CutWord(doc_info.content, &content_tokens);
 
-        for(string word : title_tokens)
+        for(string word : content_tokens)
         {
             boost::to_lower(word);
             ++word_count_map[word].content_count;
@@ -137,36 +135,40 @@ namespace index
 
             //找到对应的倒排拉链，将权重尾插进去
             InvertedList& invert_list  = invert_index[word_pair.first];
-            invert_index.push_back(std::move(Weight));
+            invert_list.push_back(std::move(weight));
         }
     }
 
     //构建索引
-    bool Build(const string& input_path)
+    bool Index::Build(const string& input_path)
     {
         cout << "索引构建开始！！！" << endl;
         
         ifstream file(input_path.c_str());
         if(!file.is_open())
         {
-            cout << "文件打开失败" << endl;
+            cout << "raw_input 文件打开失败！！！" << endl;
             return false;
         }
 
         string line;
         while(getline(file, line))
         {
-            DocInfo* doc_info = buildForwardIndex(line);
+            DocInfo* doc_info = Index::buildForwardIndex(line);
             if(doc_info == nullptr)
             {
                 //当前文档构建正排索引失败，跳过本轮
                 cout << "正排索引构建失败！！！" << endl;
                 continue;
             }
-        }
 
-        //根据doc_info来构建倒排索引
-        buildInvertIndex(*doc_info);
+            //根据doc_info来构建倒排索引
+            Index::buildInvertIndex(*doc_info);
+
+            /*
+                抽空补充一个进度条
+            */
+        } 
 
         cout << "索引构建结束！！！" << endl;
         file.close();
@@ -174,7 +176,7 @@ namespace index
     } 
 
     //查找正排索引
-    const DocInfo* Index::getDocInfo(int64_t doc_id) const
+    DocInfo* Index::getDocInfo(int64_t doc_id) 
     {
         if(doc_id < 0 || doc_id >= forward_index.size())
         {
@@ -185,7 +187,7 @@ namespace index
     }
 
     //查找倒排索引
-    const InvertedList* Index::getInvertedList(const string& key) const
+    InvertedList* Index::getInvertedList(const string& key) 
     {
         auto it = invert_index.find(key);
         if(it == invert_index.end())
@@ -195,6 +197,6 @@ namespace index
         
         return &it->second;
     }
-};
+};  //namespace indexer
 
 
---
+++ searcher/searcher.hpp
@@ -0,0 +1,129 @@
+#include"../common/util.hpp"
+#include"../index/index.hpp"
+
+namespace searcher
+{
+    class Searcher
+    {
+        private:
+        indexes::Index* index;  //索引指针
+        string generateDesc(const string& content, const string& word);
+
+        public:
+        Searcher() : 
+            index(new indexes::Index()) //伴随整个程序运作周期，不需要delete，程序结束后自动销毁。
+        {}
+        bool Init(const string& input_path);
+        bool Search(const string& query, string* results);
+
+    };
+
+    //初始化构建索引
+    bool Searcher::Init(const string& input_path)
+    {
+        if(!index->Build(input_path))
+        {
+            cout << "搜索模块初始化失败！！！" << endl;
+            return false;
+        }
+        
+        return true;
+    }
+
+    //搜索
+    bool Searcher::Search(const string& query, string* output)
+    {
+        //1.分词：将查询字符串进行分词
+        vector<string> tokens;
+        index->CutWord(query, &tokens);
+
+        //2.触发：根据分词的结果在倒排索引中查找到相关的文档编号，再通过正排索引找到对应的文档
+        vector<indexes::Weight> all_tokens;
+        for(string word : tokens)
+        {
+            boost::to_lower(word);  //统一转为小写
+
+            auto* invert_list = index->getInvertedList(word);
+            if(invert_list == nullptr)
+            {
+                cout << "倒排索引中不存在当前词！！！" << endl;
+                continue;
+            }
+
+            all_tokens.insert(all_tokens.end(), invert_list->begin(), invert_list->end());  //将搜索到的倒排拉链加入结果集中
+        }
+
+        //3.排序：根据权重信息来对Weight对象进行排序，将权重较高的高相关度文档放在前面
+        std::sort(all_tokens.begin(), all_tokens.end(), [](const indexes::Weight& w1, const indexes::Weight& w2)->bool{
+            return w1.weight > w2.weight;
+        });
+
+        //4.包装
+        Json::Value results;    //以cppJson包装的结果集
+        for(const auto& weight : all_tokens)
+        {
+            const auto* doc_info = index->getDocInfo(weight.doc_id);
+            if(doc_info == nullptr)
+            {
+                cout << "该文档ID:" << weight.doc_id << "不存在" << endl;
+                continue;
+            }
+
+            Json::Value result;
+            result["title"] = doc_info->title;
+            result["url"] = doc_info->url;
+            result["desc"] = generateDesc(doc_info->content, weight.word);
+
+            results.append(result);
+        }
+
+        //将得到的Json对象序列化为字符串，存入output中
+        Json::StreamWriterBuilder builder;
+        //std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+        *output = Json::writeString(builder, results);
+        //writer->write(results, *output);
+
+        return true;
+    }
+
+    //从正文中提取描述信息
+    string Searcher::generateDesc(const string& content, const string& word)
+    {
+        /*
+        ·   （确保关键词尽量在前，描述更加准确。并且保证描述信息简短，如果后面还有数据则用省略号表示）
+            描述信息构建规则
+            1.正常情况下，选择关键词位置的前50个字节的位置作为起始位置，返回起始位置往后的160个字节。
+            2.当前面不足50个时，就从起点位置往后直接返回150个字节。当末尾不足时，直接返回到结束位置。如果后面还有数据用省略号表示。
+            3.如果正文中未找到关键词，则说明关键词在标题中，直接从起始位置开始返回160字节
+        */
+
+        size_t word_pos = content.find(word);
+        size_t begin_pos = 0;
+
+        if(word_pos == string::npos)
+        {
+            //此时说明关键词位于标题，直接返回160个字节的数据，如果长度不够则直接返回正文
+            if(content.size() < 160)
+            {
+                return content;
+            }
+            
+            string desc = content.substr(begin_pos, 157);
+            desc += "...";
+
+            return desc;
+        }
+
+        begin_pos = (word_pos > 50) ? word_pos - 50 : 0;
+        //如果后续不够则直接返回到结尾
+        if(begin_pos + 160 >= content.size())
+        {
+            return content.substr(begin_pos);
+        }
+
+        string desc = content.substr(begin_pos, 157);
+        desc += "...";
+
+        return desc;
+    }
+};  //namespace searcher
